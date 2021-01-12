47fb74e48a88b4055351b50fa65ad795dae92175
timagr615 <timagr615@gmail.com>
2021-01-12 16:02:38 +0300

    Изменён parser_config.py

--- parser/parser_config.py
+++ parser/parser_config.py
@@ -1,5 +1,7 @@
 year = 2021
 url_470 = f"http://www.470.org/default/events/index/month/0/year/{year}"
+url_ilca = 'https://sailing.laserinternational.org/public/site/event-list'
+url_49er = 'https://49er.org/event/'
 
 month_dict = {
     'Jan': 1,
@@ -14,4 +16,19 @@ month_dict = {
     'Oct': 10,
     'Nov': 11,
     'Dec': 12
+}
+
+month_dict_49 = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
 }
